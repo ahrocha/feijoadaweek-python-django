6df066976196b5093d102907b43cf9679b1ca6ea
Andrey Rocha <ahrocha@gmail.com>
2025-05-27 21:20:00 -0300

    Homepage listing all posts.

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+from .models import Post
+
+class HomePageView(ListView):
+    model = Post
+    template_name = 'core/home.html'
+    context_object_name = 'posts'
+    ordering = ['-published_at']
