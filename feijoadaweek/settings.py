a67922b3fb3feaae0d44d49994b99b4bc0df25aa
Andrey Rocha <ahrocha@gmail.com>
2025-05-28 14:43:24 -0300

    core: static files configuration.

--- feijoadaweek/settings.py
+++ feijoadaweek/settings.py
@@ -144,3 +144,9 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 TAILWIND_APP_NAME = 'theme'
+
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
