ceaf1cd4223a4338db7cedfe57c70f79b79a5c0f
Andrey Rocha <ahrocha@gmail.com>
2025-06-09 13:17:31 -0300

    core: start-up script.

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Coleta os arquivos estáticos
+echo ">> Coletando arquivos estáticos..."
+python manage.py collectstatic --noinput
+
+# Inicia o servidor com Gunicorn
+echo ">> Iniciando aplicação com Gunicorn..."
+gunicorn feijoadaweek.wsgi:application --workers 4 --bind 0.0.0.0:8080
