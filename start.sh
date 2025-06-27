1a577e1596133beb71a62ea16afc3fbfcb787446
Andrey Rocha <ahrocha@gmail.com>
2025-06-27 12:41:14 -0300

    core: added --limit-request-field_size 16384 to gunicorn.

--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ python manage.py collectstatic --noinput
 
 # Inicia o servidor com Gunicorn
 echo ">> Iniciando aplicação com Gunicorn..."
-gunicorn feijoadaweek.wsgi:application --workers 4 --bind 0.0.0.0:8080
+gunicorn feijoadaweek.wsgi:application --workers 4 --bind 0.0.0.0:8080 --limit-request-field_size 16384
