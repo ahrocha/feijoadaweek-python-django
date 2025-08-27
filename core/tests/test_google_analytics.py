dc2be2b10b9b62611b4febd8a122bee11e287a42
Andrey Rocha <ahrocha@gmail.com>
2025-08-27 10:12:27 -0300

    feat: add tests to check for Google Analytics code.

---
+++ core/tests/test_google_analytics.py
@@ -0,0 +1,15 @@
+from django.test import Client, TestCase
+from django.templatetags.static import static
+
+
+class FaviconViewTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_homepage_has_google_analytics(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+        self.assertContains(response, "G-2SRKG8M8X1")
+        self.assertContains(response, "gtag")
