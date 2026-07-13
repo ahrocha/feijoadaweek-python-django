e5c86f6009961c16f5c7c26230e90d138d27fa27
Andrey <ahrocha@gmail.com>
2026-07-13 20:52:08 -0300

    feat: html do detalhe do local de feijoada.

--- feijoadaweek/urls.py
+++ feijoadaweek/urls.py
@@ -19,7 +19,7 @@ from django.contrib.sitemaps.views import sitemap
 from feijoadaweek.otherfiles import adstxt
 from feijoadaweek.sitemaps import PostSitemap, StaticViewSitemap
 from django.urls import include, path, re_path
-from core.views import HistoriaPageView, HomePageView, PostDetailView, SobrePageView, ContatoPageView, FeijoadaTodosOsDiasPageView, MapaDasFeijoadasPageView
+from core.views import HistoriaPageView, HomePageView, PostDetailView, SobrePageView, ContatoPageView, FeijoadaTodosOsDiasPageView, MapaDasFeijoadasPageView, MapaDasFeijoadasDetailView
 from django.conf import settings
 from django.conf.urls.static import static
 from django.templatetags.static import static as static_tag
@@ -38,6 +38,7 @@ urlpatterns = [
     path('sobre/', SobrePageView.as_view(), name='sobre'),
     path('historia-da-feijoada/', HistoriaPageView.as_view(), name='historia_da_feijoada'),
     path('mapa-das-feijoadas/', MapaDasFeijoadasPageView.as_view(), name='mapa_das_feijoadas'),
+    path('restaurante-de-feijoada/', MapaDasFeijoadasDetailView.as_view(), name='mapa_das_feijoadas_detail'),
     path('feijoada-todos-os-dias/', FeijoadaTodosOsDiasPageView.as_view(), name='feijoada_todos_os_dias'),
     path('contato/', ContatoPageView.as_view(), name='contato'),
     path('restaurante/<slug:slug>/', PostDetailView.as_view(), name='post_detail'),
